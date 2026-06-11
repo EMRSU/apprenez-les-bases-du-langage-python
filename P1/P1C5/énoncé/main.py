09a668c877d5cb9c8606cec82c8efd11fa0cb67b
Ursem NGOUFACK KENFACK <ukenfack@cirilgroup.com>
2026-06-11 12:09:01 +0200

    type de variable

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
 # Écrivez votre code ici !
+nom = "John DOE"
+age = 33
+taille = 1.80
+est_etudiant = False
+print(f"Nom : {nom}")
+print("Age", age)
+print(f"Taille : {taille}")
+print("Etudiant : ", est_etudiant)
+#type de donnée
+print(f"Type Nom :", type(nom))
+print("Type Age", type(age))
+print(f"Type Taille : {type(taille)}")
+print("Type Etudiant : ", type(est_etudiant))
