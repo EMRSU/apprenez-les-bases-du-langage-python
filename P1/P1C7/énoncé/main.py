f98d0a82837808a5ca8067a1d37c4e07b237732b
Ursem NGOUFACK KENFACK <ukenfack@cirilgroup.com>
2026-09-10 16:49:29 +0200

    dictionnaire de donnée

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+#Question1
+fruits = {
+    "pomme" : "rouge",
+    "banane" : "jaune",
+    "orange" : "orange" 
+    }
+#Question2
+fruits["kiwi"] = "vert"
+#Question3
+couleur_banane = fruits["banane"]
+#Question4
+fruits["pomme"] = "vert"
+#Question5
+del fruits ["banane"]
+#Question6
+print(fruits.keys())
