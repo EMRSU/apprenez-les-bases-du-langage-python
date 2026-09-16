6eda98f61ff0e7a0847062b6e37a4eddc487402a
Ursem NGOUFACK KENFACK <ukenfack@cirilgroup.com>
2026-09-16 17:23:54 +0200

    openclass room: condition

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,37 @@
 # Ecrivez votre code ici !
+#Question 1:
+nombre1 = input("Entrer un nombre ")
+nombre2 = input("Entrer un second nombre ")
+
+#Question 2, 3 & 4:
+if not nombre1.isnumeric() and not nombre2.isnumeric():
+    print("les deux nombre doivent être des entier")
+    raise SystemExit("Fin du programme")
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
+
+#Question 5:
+operation = input("Entrez l'opération souhaitée ('+', '-', '*' ou '/') ")
+
+#Question 6:
+if operation not in ["+", "-", "*", "/"]:
+    print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'.")
+    raise SystemExit("Fin du programme")
+
+#Question 7, 8 & 9:
+if operation == "+":
+    resultat = nombre1 + nombre2
+elif operation == "-":
+    resultat = nombre1 - nombre2
+elif operation == "*":
+    resultat = nombre1 * nombre2
+elif operation == "/":
+    # Vérifie si la variable `nombre2` n'est pas nulle pour la division
+    if nombre2 == 0:
+        print("Erreur: impossible de diviser par zéro.")
+        raise SystemExit("Fin du programme")
+
+    resultat = round(nombre1 / nombre2, 2)
+
+#Question 10:
+print(f"Le résultat de l'opération est: {round(resultat, 2)}")
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,33 @@
 print("Hello, world")
+
+ensoleille = False
+neige = True
+if ensoleille:
+    print("on va à la plage !")
+elif neige:
+    print("on fait un bonhomme de neige")
+else:
+    print("on reste à la maison !")
+
+avec_soleil = True
+en_semaine = False
+if avec_soleil and not en_semaine:
+    print("on va à la plage !")
+elif avec_soleil and en_semaine:
+    print("on va au travail !")
+else:
+    print("on reste à la maison !")
+
+nombre_de_sieges = 30
+nombre_dinvites = 25
+
+fruit = "pomme"
+match fruit:
+    case "pomme":
+        print("J'aime les pommes !")
+    case "banane":
+        print("Je n'aime pas les bananes.")
+    case "orange":
+        print("Les oranges sont bonnes pour la santé.")
+    case _:
+        print("Je ne connais pas ce fruit.")
